71e2e522ae1b4c870cde3b7b365e1cf34822db79
nuochv <nuochv@gmail.com>
2021-12-01 21:07:22 +0700

    first commit

---
+++ supportportal/src/main/java/com/supportportal/service/ipml/UserServiceImpl.java
@@ -0,0 +1,51 @@
+package com.supportportal.service.ipml;
+
+import com.supportportal.constant.UserImplConstant;
+import com.supportportal.domain.User;
+import com.supportportal.domain.UserPrincipal;
+import com.supportportal.repository.UserRepository;
+import com.supportportal.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+import java.util.Date;
+
+import static com.supportportal.constant.UserImplConstant.*;
+
+
+@Service
+@Transactional
+@Qualifier("userDetailsService")
+public class UserServiceImpl implements UserService, UserDetailsService {
+    private UserRepository userRepository;
+    private Logger LOGGER = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findUserByUsername(username);
+        if(user == null){
+            LOGGER.error(NO_USER_FOUND_BY_USERNAME + username);
+            throw new UsernameNotFoundException(NO_USER_FOUND_BY_USERNAME + username);
+        } else {
+            user.setLastLoginDateDisplay(user.getLastLoginDate());
+            user.setLastLoginDate(new Date());
+            userRepository.save(user);
+            UserPrincipal userPrincipal = new UserPrincipal(user);
+            LOGGER.info(FOUND_USER_BY_USERNAME + username);
+            return userPrincipal;
+        }
+    }
+}
